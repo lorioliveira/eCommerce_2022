c1d4f7866f6c068a4ea9045dd7561030018d5458
Lorena <aneroliver@gmail.com>
2022-02-20 14:43:57 -0300

    arquivos deletados

--- WebContent/js/esconderform.js
+++
@@ -1,7 +0,0 @@
-function Mudarestado(el) {
-        var display = document.getElementById(el).style.display;
-        if(display == "block")
-            document.getElementById(el).style.display = 'none';
-        else
-            document.getElementById(el).style.display = 'block';
-    }
