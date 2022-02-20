c182608d654461f621e3037faf92c55c1d5f9dbe
Lorena <aneroliver@gmail.com>
2022-02-20 14:46:53 -0300

    novos arquivos

---
+++ WebContent/js/all.js
@@ -0,0 +1,52 @@
+// MÁSCARA PARA O CAMPO DO TELEFONE - TELA "MEUS DADOS / REGISTRAR NOVA CONTA"
+//
+const tel = document.getElementById('telefone') // Seletor do campo de telefone
+
+tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) // Dispara quando digitado no campo
+tel.addEventListener('change', (e) => mascaraTelefone(e.target.value)) // Dispara quando autocompletado o campo
+
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "")
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
+    tel.value = valor // Insere o(s) valor(es) no campo
+}
+
+//
+// MÁSCARA PARA O CAMPO DO CPF DO CLIENTE - TELA "REGISTRAR NOVA CONTA"
+//
+
+function ValidaCPF(){	
+	var RegraValida=document.getElementById("RegraCPF").value; 
+	var cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;	 
+}
+  function fMasc(objeto,mascara) {
+obj=objeto
+masc=mascara
+setTimeout("fMascEx()",1)
+}
+
+  function fMascEx() {
+obj.value=masc(obj.value)
+}
+
+   function mCPF(cpf){
+cpf=cpf.replace(/\D/g,"")
+cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+return cpf
+}
+
+//
+// MÁSCARA PARA O CAMPO DE CEP - TELA "NOVO ENDERECO"
+//
+
+function mascara(t, mask){
+  var i = t.value.length;
+  var saida = mask.substring(1,0);
+  var texto = mask.substring(i)
+  if (texto.substring(0,1) != saida){
+    t.value += texto.substring(0,1);
+  }
+}
